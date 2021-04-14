a7f99b339f6d2ffebfdafe4f86d1eb534e891c38
MaaikeG <maaikegalama@outlook.com>
2021-04-14 20:58:19 +0200

    Added batch-wise evaluation of the data. This destroys everything.

--- examples/run_estimator.py
+++ examples/run_estimator.py
@@ -18,7 +18,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--potential", default='double_well', help="The potential function")
     parser.add_argument("-d", "--n_dimensions", default=1, help="The number of dimensions to sample from")
-    parser.add_argument("-n", "--n_samples", default=10000, help="The number of samples per simulation")
+    parser.add_argument("-n", "--n_samples", default=1000, help="The number of samples per simulation")
     parser.add_argument("-m", "--n_bins", default=100, help="The number of histogram buckets (in case of WHAM)")
     parser.add_argument("-b", "--n_biases", default=10, help="The number of bias potentials")
     parser.add_argument("--hist_min", default=0, help="Minimum of the leftmost histogram bin")
@@ -35,7 +35,7 @@ def main():
     # Generate data for the potential using the MCMC sampling method
     sampler = MCMC.MCMC(args)
     data = np.asarray(sampler.sample(U, biases)).flatten()
-    data = torch.tensor(np.histogram(data, range=(args.hist_min, args.hist_max), bins=args.n_bins)[0], dtype=float)
+    # data = torch.tensor(np.histogram(data, range=(args.hist_min, args.hist_max), bins=args.n_bins)[0], dtype=float)
 
     # instantiate estimator and optimizers
     estimator = estimators.wham.WHAM(biases, args.n_bins)
@@ -44,7 +44,8 @@ def main():
 
     # estimate free energy based on sampled data and plot
     for (optimizer, label) in [(SGD, "SGD"), (ADAM, "ADAM")]:
-        plt.plot(estimate_free_energy(estimator, optimizer, data).detach().numpy(), label=label)
+        estimate_free_energy(estimator, optimizer, data, n_batches=10, args=args)
+        # plt.plot(estimate_free_energy(estimator, optimizer, data, n_batches=1, args=args).detach().numpy(), label=label)
 
     plt.plot(U(range(args.hist_max)), label="Real potential function")
     plt.legend()
@@ -52,13 +53,26 @@ def main():
 
 
 
-def estimate_free_energy(estimator, optimizer, data):
-    for i in range(1000):
-        optimizer.zero_grad()
-        loss = estimator.residue(data)
-        print(loss.item())
-        loss.backward()
-        optimizer.step()
+def estimate_free_energy(estimator, optimizer, data, n_batches, args):
+    losses = []
+    batch_size = int(len(data)/n_batches)
+
+    for i in range(100):
+        np.random.shuffle(data)
+
+        for b in range(n_batches):
+            batch_data = torch.tensor(
+                np.histogram(data[b*batch_size:(b+1)*batch_size], range=(args.hist_min, args.hist_max), bins=args.n_bins)[0],
+                dtype=float)
+
+            optimizer.zero_grad()
+            loss = estimator.residue(batch_data)
+            losses.append(loss.item())
+            loss.backward()
+            optimizer.step()
+
+    plt.plot(losses)
+    plt.show()
     return estimator.get_free_energy()
 
 
