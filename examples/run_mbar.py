3e3ad3ea076abdae8ea237fb5b89b272932aba03
MaaikeG <maaikegalama@outlook.com>
2021-04-25 15:11:05 +0200

    expectation value works

--- examples/run_mbar.py
+++ examples/run_mbar.py
@@ -2,9 +2,9 @@ import argparse
 import thermodynamicestimators.utilities.test_problem_factory as problem_factory
 import thermodynamicestimators.estimators.MBAR as mbar
 import matplotlib.pyplot as plt
-import numpy as np
 import torch
 from mpl_toolkits.mplot3d import Axes3D
+from pylab import *
 
 
 """
@@ -17,7 +17,7 @@ Uses WHAM to return an estimate of the potential function.
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--test_name", default='double_well_1D', help="The name of the test problem")
-    parser.add_argument("--tolerance", default=10e-2, help="Error tolerance for convergence")
+    parser.add_argument("--tolerance", default=1e-4, help="Error tolerance for convergence")
     parser.add_argument("--max_iterations", default=200, help="Maximum number of iterations allowed to converge")
 
     args = parser.parse_args()
@@ -26,33 +26,29 @@ def main():
     # generate a test problem with potential, biases, data and histogram bin range
     test_problem = problem_factory.make_test_problem(args.test_name)
 
-
     estimator = mbar.MBAR(test_problem.data_at_all_states)
-
-
     optimizer_SGD = torch.optim.SGD(estimator.parameters(), lr=0.001)
-    potential_SGD, errors_SGD = estimate_free_energy(estimator,
+    free_energy_SGD, errors_SGD = estimate_free_energy(estimator,
                                              optimizer_SGD,
                                             test_problem.data_at_all_states,
                                      args=args)
 
+    plt.yscale('log')
+    plt.ylabel(r'max_i (F(t)_i - F(t-1)_i)^2 / |avg(F(t))|$')
+    plt.xlabel(r'Epoch t')
+    plt.plot(errors_SGD, label="error")
+    plt.show()
+
+
     def bin_sample(x):
         hist = [0] * 100
-        hist[x]=1
+        hist[int(x)]=1
         return hist
 
-    probability_SGD = estimator.get_expectation_value(test_problem.data, test_problem.data_at_all_states, bin_sample)
+    potential_SGD = -np.log(estimator.get_expectation_value(test_problem.data, test_problem.data_at_all_states, bin_sample).detach())
 
-
-    # plt.yscale('log')
-    # plt.plot(errors_SGD, label='SGD error, lr=0.1')
-
-    # plt.legend()
-    # plt.show()
-
-
-    # plt.plot(test_problem.potential(range(100)), label="real potential function", color='g')
-    plt.plot(-np.log(probability_SGD.detach().numpy()), label="SGD")
+    plt.plot(potential_SGD, label="Real potential function")
+    plt.plot(test_problem.potential(range(100)), label="SGD, lr=0.001")
 
     plt.legend()
     plt.show()
@@ -66,7 +62,7 @@ def estimate_free_energy(estimator, optimizer, data, args):
     free_energy = 0
     errors = []
 
-    while epoch < args.max_iterations:# and error > args.tolerance:
+    while epoch < args.max_iterations and error > args.tolerance:
 
         epoch += 1
 
@@ -75,24 +71,17 @@ def estimate_free_energy(estimator, optimizer, data, args):
         loss.backward()
         optimizer.step()
 
-        estimator.self_consistent_step(data)
-
 
         with torch.no_grad():
-            with torch.no_grad():
-                estimator._f -= estimator._f[0].clone()
-                estimator.G -= estimator.G[0].clone()
+            estimator._f -= estimator._f[0].clone()
 
-            error = torch.abs(torch.max(torch.square(estimator.free_energy - free_energy) / estimator.free_energy.mean()))
+            error = torch.max(torch.square((estimator.free_energy - free_energy) / torch.abs(estimator.free_energy.mean())))
             free_energy = estimator.free_energy
+
         print(error)
         errors.append(error)
 
-        if epoch % 100 ==0:
-            plt.plot(estimator.free_energy)
-            plt.plot(estimator.G)
-    plt.show()
-    return estimator.free_energy.numpy(), errors
+    return estimator.free_energy, errors
 
 if __name__ == "__main__":
     main()
--- thermodynamicestimators/estimators/MBAR.py
+++ thermodynamicestimators/estimators/MBAR.py
@@ -1,30 +1,19 @@
 from thermodynamicestimators.estimators.thermodynamicestimator import ThermodynamicEstimator
 import torch
-import math
-import pymbar
 
 
 class MBAR(ThermodynamicEstimator):
     def __init__(self, data):
         super().__init__()
         self.n_states = len(data)
-
-        self.G = torch.zeros(self.n_states, dtype=torch.float64)
-        # self.self_consistent_step(data)
-        # self.self_consistent_step(data)
-
-        self._f = torch.nn.Parameter(self.G.clone())
+        self._f = torch.nn.Parameter(torch.zeros(self.n_states, dtype=torch.float64))
 
     '''free energy estimate per bias '''
     @property
     def free_energy(self):
         # TODO: make N_i/N variable
-        return self._f.detach()
+        return self._f.detach().clone()
 
-    def free_energy_test(self, data):
-        N = data.shape[1]
-        log_denom = torch.sum(torch.exp(-data + self._b.detach().unsqueeze(1)))
-        return -torch.log(1/N * torch.sum(torch.exp(-data + self._b.detach().unsqueeze(1))/log_denom, axis=1))
 
     '''get the partition function for each (biased) thermodynamic state'''
     @property
@@ -34,7 +23,7 @@ class MBAR(ThermodynamicEstimator):
 
     '''get the unbiased partition function'''
     def get_unbiased_partition_function(self, data):
-        return torch.sum(1/self.get_sample_weights(data))
+        return torch.sum(torch.exp(-data) * self.get_sample_weights(data))
 
 
     '''Get the weights of all samples.
@@ -45,22 +34,32 @@ class MBAR(ThermodynamicEstimator):
         # TODO: make variable
         N_i = 1000
 
-        return 1/ torch.sum(((N_i/self.partition_functions) * torch.exp(-data).T).T, axis=0)
+        return 1/ torch.sum(((N_i/self.partition_functions) * torch.exp(-data).T), axis=1)
 
 
     '''Get the expectation value of an observable function based on the observed data'''
-    def get_expectation_value(self, sample_positions, sample_potentials, observable_function):
-        sample_positions = torch.flatten(sample_positions)
-        result_shape = torch.tensor(observable_function(1)).shape
-        values = torch.zeros(sample_positions.shape + result_shape)
+    def get_expectation_value(self, samples, sample_potentials, observable_function):
 
-        for s_i in range(len(sample_positions)):
-            values[s_i] = torch.tensor(observable_function(sample_positions[s_i]))
+        samples = samples.flatten()
+
+        # construct a matrix to store the computed observables
+        result_shape = torch.tensor(observable_function(1)).shape
+        observable_values = torch.zeros(samples.shape + result_shape)
+
+        # fill it with the observed values
+        for s_i in range(len(samples)):
+            observable_values[s_i] = torch.tensor(observable_function(samples[s_i].item()))
+
+        # return the expectation value (sum of observables weighted with the sample probabilities)
+        return torch.sum(self.get_weighted_observables(observable_values, sample_potentials), axis=1)
+
+
+    ''' Weight the observed values by multiplying with the sample probabilities '''
+    def get_weighted_observables(self, observable_values, sample_potentials):
 
         # weigh each observed value with the probability of the sample
-        res = (1 / self.get_unbiased_partition_function(sample_potentials)) * \
-            torch.sum(values.T/self.get_sample_weights(sample_potentials), axis=1)
-        return res
+        return observable_values.T * self.get_sample_weights(sample_potentials) /\
+               self.get_unbiased_partition_function(sample_potentials)
 
 
     '''
@@ -68,6 +67,8 @@ class MBAR(ThermodynamicEstimator):
     data has shape: [M, N] where M is the number of thermodynamic states, and N the total number of observed samples.
     data[m,n] is sample n evaluated at state m.
     This returns the value of equation (7) in paper: 10.1021/acs.jctc.8b01010
+    Note: eq. (7) is f as a function of {b_1, ..., b_M}. Here f is written as a function of {G*_1,... G*_M}
+    Additive constants are ignored since they don't affect the gradient.
      '''
     def residue(self, data):
 
@@ -84,11 +85,10 @@ class MBAR(ThermodynamicEstimator):
 
         objective_function = (torch.sum(logsum) - torch.sum(self._f * N_i))
 
-
         return objective_function
 
 
-
+    ''' Update the free energies by calculating the self-consistent equation (4) in '''
     def self_consistent_step(self, data):
         N_l = 1000
 
