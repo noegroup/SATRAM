0d370245ba23de94fd4c2f49b2e878e6c7261ebb
MaaikeG <maaikegalama@outlook.com>
2021-04-26 15:50:57 +0200

    build fix attempt

--- thermodynamicestimators/__init__.py
+++ thermodynamicestimators/__init__.py
@@ -7,7 +7,7 @@ A short description of the project.
 from thermodynamicestimators.estimators.WHAM import *
 
 # Handle versioneer
-from ._version import get_versions
+from _version import get_versions
 versions = get_versions()
 __version__ = versions['version']
 __git_revision__ = versions['full-revisionid']
