511fceed0c15a1a9e019d92c04c4427291be35fa
MaaikeG <maaikegalama@outlook.com>
2021-04-30 11:54:10 +0200

    Attempt to fix the CI

--- thermodynamicestimators/_version.py
+++ thermodynamicestimators/_version.py
@@ -43,7 +43,7 @@ def get_config():
     cfg.style = "pep440"
     cfg.tag_prefix = ""
     cfg.parentdir_prefix = "None"
-    cfg.versionfile_source = "SGD_free_energy_estimators/_version.py"
+    cfg.versionfile_source = "thermodynamicestimators/_version.py"
     cfg.verbose = False
     return cfg
 
