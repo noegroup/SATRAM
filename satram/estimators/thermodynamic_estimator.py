add alanine dipeptide example add computation of PMF. fix some bugs

satram/estimators/thermodynamic_estimator.py
@@ -37,6 +37,8 @@ class ThermodynamicEstimator():
         self._prev_f_therm = None
         self._prev_stat_vec = None
 
+        self.dataset = None
+
 
     @property
     def free_energies(self):
@@ -54,8 +56,8 @@ class ThermodynamicEstimator():
         return compute_f_therm(self._f).cpu()
 
 
-    def _initialize_f(self, dataset):
-        self._f += dataset.dataloader.dataset[:, : dataset.n_therm_states].mean(axis=0).to(self.device)[:, None]
+    def _initialize_f(self):
+        self._f += self.dataset.dataloader.dataset[:, : self.dataset.n_therm_states].mean(axis=0).to(self.device)[:, None]
 
 
     def _get_iteration_error(self):
@@ -77,8 +79,32 @@ class ThermodynamicEstimator():
         self._prev_stat_vec = torch.zeros([n_therm_states, n_markov_states], dtype=torch.double)
 
 
+    @property
+    def sample_weights(self):
+        if self.dataset is not None:
+            _, log_R = compute_v_R(self._f, self._log_v, self.dataset.log_C_sym, self.dataset.state_counts,
+                               self.dataset.log_N)
+            return torch.logsumexp(compute_sample_weights(self._f, log_R, self.dataset.dataloader, device=self.device), 1)
+        else:
+            return None
+
+
+    def compute_pmf(self, binned_trajs, n_bins):
+        weights = self.sample_weights
+
+        pmf = torch.zeros(n_bins)
+
+        for i in range(len(pmf)):
+            indices = torch.where(torch.Tensor(binned_trajs) == i)
+            if len(indices[0]) > 0:
+                pmf[i] = -torch.logsumexp(-weights[indices], 0)
+            else:
+                pmf[i] = float("Inf")
+        return pmf - pmf.min()
+
+
     def fit(self, data, callback=None, solver_type='SATRAM', initial_batch_size=256,
-            batch_size_increase=None, delta_f_max=1., *args, **kwargs):
+            batch_size_increase=None, delta_f_max=1.):
         """Estimate the free energies.
 
         Parameters
@@ -94,24 +120,26 @@ class ThermodynamicEstimator():
                                                        batch_size_increase=batch_size_increase,
                                                        total_dataset_size=len(data))
 
-        dataset = Dataset(data, state_counts, transition_counts, device=self.device,
-                          batch_size=implementation_manager.batch_size, is_stochastic=implementation_manager.is_stochastic)
+        self.dataset = Dataset(data, state_counts, transition_counts, device=self.device,
+                               batch_size=implementation_manager.batch_size,
+                               is_stochastic=implementation_manager.is_stochastic)
 
         self._initialize_results_matrices(state_counts.shape[0], state_counts.shape[1])
-        self._initialize_f(dataset)
+        self._initialize_f()
 
         for i in self.progress(range(self.maxiter)):
 
-            self._f, self._log_v = implementation_manager.solver(dataset, self._f, self._log_v,
+            self._f, self._log_v = implementation_manager.solver(self.dataset, self._f, self._log_v,
                                                                  lr=implementation_manager.learning_rate,
                                                                  batch_size=implementation_manager.batch_size,
                                                                  delta_f_max=delta_f_max)
 
+            print(self.dataset.dataloader.batch_size)
             error = self._get_iteration_error()
 
             if implementation_manager.step(i):
-                dataset.init_dataloader(
-                    max(implementation_manager.batch_size, implementation_manager.batch_size_memory_limit),
+                self.dataset.init_dataloader(
+                    min(implementation_manager.batch_size, implementation_manager.batch_size_memory_limit),
                     implementation_manager.is_stochastic)
 
             if i % self.callback_interval == 0 and callback is not None:

satram/util/dataset.py
@@ -24,11 +24,11 @@ class Dataset:
             self.log_C_sym = torch.log(transition_counts + torch.transpose(transition_counts, 1, 2)).to(device)
 
         self.init_dataloader(batch_size, is_stochastic)
-
-
+    
     @property
     def dataloader(self):
         return self._dataloader
+
 
     def init_dataloader(self, batch_size, is_stochastic):
         self._dataloader = torch.utils.data.DataLoader(self._data, batch_size=batch_size,