- Fix notebook for Alanine Dipeptide. Data from FTP. - Fix PMF calculation

examples/datasets/toy_problem.py
@@ -1,6 +1,5 @@
 import math
 import torch
-
 import scipy.spatial
 
 
@@ -35,15 +34,28 @@ def OU_simulation(mu, sigma2, b, delta_t, T):
 lag=1
 
 
-def generate_data():
+def get_data():
     trajs = []
+    energies = []
+
+    for i in range(n_therm_states):
+        traj = OU_simulation(mu[i], sigma2, 1, 10, T)
+
+        trajs.append(traj)
+        energies.append((traj - mu) ** 2 / (2 * sigma2))
+
+    return trajs, energies
+
+
+def get_tram_input():
+    trajs, energies = get_data()
+
     ttrajs = []
     dtrajs = []
     bias_list = []
 
     for i in range(n_therm_states):
-        traj = OU_simulation(mu[i], sigma2, 1, 10, T)
-
+        traj = trajs[i]
         tmp_d = torch.Tensor(scipy.spatial.distance.cdist(traj, centers) ** 2)
         dtraj = torch.argmin(tmp_d, 1)
 
@@ -51,7 +63,9 @@ def generate_data():
         dtrajs.append(dtraj)
         ttrajs.append(torch.Tensor([i] * T))
 
-        bias = (traj - mu) ** 2 / (2 * sigma2) + s
+        bias = energies[i] + s
         bias_list.append(bias)
 
-    return trajs, ttrajs, dtrajs, bias_list
+    return ttrajs, dtrajs, bias_list
+
+

satram/estimators/thermodynamic_estimator.py
@@ -84,7 +84,8 @@ class ThermodynamicEstimator():
         if self.dataset is not None:
             _, log_R = compute_v_R(self._f, self._log_v, self.dataset.log_C_sym, self.dataset.state_counts,
                                self.dataset.log_N)
-            return torch.logsumexp(compute_sample_weights(self._f, log_R, self.dataset.dataloader, device=self.device), 1)
+            return torch.logsumexp(compute_sample_weights(self._f, log_R, self.dataset.deterministic_dataloader,
+                                                          device=self.device), 1)
         else:
             return None
 
@@ -97,7 +98,7 @@ class ThermodynamicEstimator():
         for i in range(len(pmf)):
             indices = torch.where(torch.Tensor(binned_trajs) == i)
             if len(indices[0]) > 0:
-                pmf[i] = -torch.logsumexp(-weights[indices], 0)
+                pmf[i] = -torch.logsumexp(weights[indices], 0)
             else:
                 pmf[i] = float("Inf")
         return pmf - pmf.min()
@@ -146,3 +147,5 @@ class ThermodynamicEstimator():
 
             if error < self.maxerr:
                 return
+
+            i += 1

satram/util/data_handler.py
@@ -45,10 +45,9 @@ def process_input(data, lagtime):
         ind_traj = torch.zeros((len(traj), n_markov_states))
         sample_idx = torch.arange(0, len(traj))
 
-        ind_traj[(sample_idx, traj)] = 1
+        ind_traj[(sample_idx.long(), traj.long())] = 1
 
         C = torch.tensordot(ind_traj[:-lagtime].T, ind_traj[lagtime:], 1)
-        # C = ind_traj[:-lagtime].T.dot(ind_traj[lagtime:])
         transition_counts[i] = C
         state_counts[i] = ind_traj.sum(0)
 

satram/util/dataset.py
@@ -29,7 +29,10 @@ class Dataset:
     def dataloader(self):
         return self._dataloader
 
-
     def init_dataloader(self, batch_size, is_stochastic):
         self._dataloader = torch.utils.data.DataLoader(self._data, batch_size=batch_size,
                                                        drop_last=is_stochastic, shuffle=is_stochastic)
+
+    @property
+    def deterministic_dataloader(self):
+        return torch.utils.data.DataLoader(self._data, batch_size=1024, drop_last=False, shuffle=False)